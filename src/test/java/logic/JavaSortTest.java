862375c7cb34ba1d8353b8a2b3226a70dc27b1df
mingsang <minsang0850@naver.com>
2022-02-08 14:49:53 +0900

    before checkout develop

---
+++ src/test/java/logic/JavaSortTest.java
@@ -0,0 +1,23 @@
+package logic;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class JavaSortTest {
+    private JavaSort<Integer> javaSort = new JavaSort();
+    @DisplayName("데이터 리스트를 입력하면, 정렬된 리스트를 반환한다")
+    @Test
+    void givenList_whenSorting_thenReturnSortedList() {
+        //Given
+        List<Integer> input = List.of(5,3,1,4,2);
+        
+        //When
+        List<Integer> output = javaSort.sort(input);
+        
+        //Then
+        assertThat(output).isEqualTo(List.of(1,2,3,4,5));
+    }
+}
